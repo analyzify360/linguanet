72b68b2abab7d549f6295c0de1c6ba62f602f0b9
mltrev23 <trevor.dev23@gmail.com>
2024-12-16 10:53:50 -0600

    feat: both cli for miner and validator

---
+++ src/subnet/miner/cli.py
@@ -0,0 +1,39 @@
+import typer
+import getpass
+from typing import Annotated
+from communex.compat.key import classic_load_key
+from keylimiter import TokenBucketLimiter
+from communex.module.server import ModuleServer
+import uvicorn
+import os
+from dotenv import load_dotenv
+
+from miner.miner import Miner
+
+load_dotenv()
+
+app = typer.Typer()
+
+@app.command("serve-subnet")
+def serve(
+    commune_key: str,
+    netuid: int = 30,
+    ip: str = typer.Option("0.0.0.0", help="IP to bind the server to"),
+    port: int = typer.Option(9900, help="Port to bind the server to"),
+    use_testnet: bool = typer.Option(False, help="Network to connect to [`mainnet`, `testnet`]"),
+):
+    password = getpass.getpass(prompt="Enter the password for your key:")
+    key = classic_load_key(commune_key, password=password)
+    miner = Miner()
+    refill_rate = 1 / 4
+    
+    # Implementing custom limit
+    bucket = TokenBucketLimiter(15, refill_rate)
+    server = ModuleServer(miner, key, limiter=bucket, subnets_whitelist=[netuid], use_testnet = use_testnet)
+    app = server.get_fastapi_app()
+
+    # Only allow local connections
+    uvicorn.run(app, host=ip, port=port)
+
+if __name__ == "__main__":
+    typer.run(serve)
--- src/subnet/validator/cli.py
+++ src/subnet/validator/cli.py
@@ -1,4 +1,5 @@
 import typer 
+import getpass
 from typing import Annotated
 
 from communex._common import get_node_url  # type: ignore
@@ -6,27 +7,27 @@ from communex.client import CommuneClient  # type: ignore
 from communex.compat.key import classic_load_key  # type: ignore
 
 from validator._config import ValidatorSettings
-from validator.validator import get_subnet_netuid, TextValidator
+from validator.validator import TextValidator
 
 app = typer.Typer()
 
 
 @app.command("serve-subnet")
 def serve(
-    commune_key: Annotated[
-        str, typer.Argument(help="Name of the key present in `~/.commune/key`")
-    ],
+    commune_key: Annotated[str, typer.Argument(help="Name of the key present in `~/.commune/key`")],
+    netuid: int = typer.Option(1, help="Netuid of the subnet"),
+    use_testnet: bool = typer.Option(False, help="Use testnet"),
     call_timeout: int = 65,
 ):
-    keypair = classic_load_key(commune_key)  # type: ignore
+    password = getpass.getpass(prompt = "Enter the password to decrypt your key:")
+    keypair = classic_load_key(commune_key, password=password)  # type: ignore
     settings = ValidatorSettings()  # type: ignore
-    c_client = CommuneClient(get_node_url())
-    subnet_uid = get_subnet_netuid("your-subnet-name")
+    c_client = CommuneClient(get_node_url(use_testnet=use_testnet))
     validator = TextValidator(
         keypair,
-        subnet_uid,
+        netuid,
         c_client,
-        call_timeout=call_timeout,
+        call_timeout,
     )
     validator.validation_loop(settings)
 
